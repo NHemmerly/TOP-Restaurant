ec2fec555bdc007a474a90a553048b0a75cf9d17
NeilH <neilhemm0@gmail.com>
2023-01-23 20:01:25 -0500

    Add tagline

--- src/index.js
+++ src/index.js
@@ -12,12 +12,12 @@ import backgroundImage from './images/pizza-home.jpg';
 //object, and the objects nested beneath the tag. 
 const navbar = domElement(
         'nav',
-        { class: 'navbar navbar-expand-lg navbar-light p-4 justify-content-between',
+        { class: 'navbar navbar-expand-lg navbar-dark p-4 justify-content-between',
         style: 'background-color: #dc2626',
         id: 'navigation'},
     domElement(
         'a', 
-        {class: 'navbar-brand'}, 'Go Grubbie'),
+        {class: 'navbar-brand h1'}, 'The Brick Oven'),
      );
 
 const navbarList = 
@@ -46,7 +46,10 @@ pizzaImage.src = backgroundImage;
         
 const main = domElement('div', { class: 'row main'},
     domElement('div', {class: 'col background'}),
-    domElement('div', {class: 'col content'}));
+    domElement('div', {class: 'col content'},
+        domElement('div', {class: 'col'},
+            domElement('div', {class: 'row display-1 text-center'}, 'The Best Pizza Comes From The Brick Oven'),
+            domElement('div', {class: 'row display-3 text-center'}, 'Nobody does rustic aesthic with modern flavor quite like the brick oven.'))));
             
             
 navbar.appendChild(navbarList);
