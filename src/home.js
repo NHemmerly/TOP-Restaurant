c3fb23beb400fa1b3508ddbd12d5882351b8db00
NeilH <neilhemm0@gmail.com>
2023-01-24 21:32:35 -0500

    Finish homepage layout

--- src/home.js
+++ src/home.js
@@ -3,8 +3,9 @@ import { domElement } from "./factoryDOM";
 import backgroundImage from './images/pizza-home.jpg';
 
 export function renderHome() {
-        //domElement takes the tag, object containing the attributes of an 
+    //domElement takes the tag, object containing the attributes of an 
     //object, and the objects nested beneath the tag. 
+    const mainContainer = domElement('div', {class: 'holder'})
     const navbar = domElement(
         'nav',
         { class: 'navbar navbar-expand-lg navbar-dark p-4 justify-content-between',
@@ -12,7 +13,7 @@ export function renderHome() {
         id: 'navigation'},
     domElement(
         'a', 
-        {class: 'navbar-brand h1'}, 'The Brick Oven'),
+        {class: 'navbar-brand h1 athena'}, 'The Brick Oven'),
     );
 
     const navbarList = 
@@ -39,16 +40,18 @@ export function renderHome() {
     const pizzaImage = new Image();
     pizzaImage.src = backgroundImage;
         
-    const main = domElement('div', { class: 'row main'},
-    domElement('div', {class: 'col background'}),
-    domElement('div', {class: 'col content'},
-        domElement('div', {class: 'col'},
-            domElement('div', {class: 'row display-1 text-center text-light mt-2'}, 'The Best Pizza Comes From The Brick Oven.'),
-            domElement('div', {class: 'row display-3 text-center text-light mt-2'}, 'Nobody does rustic aesthetic with modern flavor quite like The Brick Oven.'))));
+    const main = domElement('div', {class: 'container-fluid p-0 main'}, 
+    domElement('div', { class: 'row two-panes m-0 h-100'},
+    domElement('div', {class: 'col background p-0'}),
+    domElement('div', {class: 'col content d-flex align-items-center p-0'},
+        domElement('div', {class: 'col tagline p-1 m-1'},
+            domElement('div', {class: 'row display-1 text-center text-light athena w-100 m-0'}, 'The Best Pizza Comes From The Brick Oven.'),
+            domElement('div', {class: 'row display-4 text-center text-light mt-5 athena w-100 m-0'}, 'Nobody does rustic aesthetic with modern flavor quite like The Brick Oven.')))));
             
             
     navbar.appendChild(navbarList);
-    document.body.appendChild(navbar);
-    document.body.appendChild(main);
+    document.body.appendChild(mainContainer)
+    mainContainer.appendChild(navbar);
+    mainContainer.appendChild(main);
 }
 
