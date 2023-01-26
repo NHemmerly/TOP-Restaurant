0b7dd9ee27c5b5819de735a84a879663ed961e87
Neil Hemmerly <neilhemm0@gmail.com>
2023-01-26 12:20:41 -0500

    Add a file to store text that will populate menu

--- src/addToDom.js
+++
@@ -1,6 +0,0 @@
-//Function for adding new Tabs to DOM
-
-export function addToDOM(tab) {
-    const content = document.getElementById('content');
-    content.appendChild(tab())
-}
---
+++ src/menu-text.js
@@ -0,0 +1,41 @@
+export const menu = [
+    {title: 'Appetizers',
+    items: [
+        {dish: 'Fried Pickles',
+        desc: 'The crunchiest fresh pickles breaded and deep-fried',
+        price: '8.00'},
+        {dish: 'Chicken Wings',
+        desc: '12 juicy chicken wings with your choice of one of our in-house wing sauces',
+        price: '10.00'},
+        {dish: 'Pretzel Bites',
+        desc: 'Soft pretzel bites with marinara sauce and cheese for dipping',
+        price: '8.00'},
+        {dish: 'Garlic Knots',
+        desc: 'Savory soft garlic knots with marinara dipping sauce',
+        price: '9.00'}
+    ]},
+    {title: 'Pastas',
+    items: [
+        {dish: 'Fettucine Chicken Alfredo',
+        desc: 'Aldente fettucine with creamy alfredo cheese sauce and chopped fire-baked chicken breast',
+        price: '12.00'},
+        {dish: 'Spaghetti with Red Sauce',
+        desc: 'Spaghetti our legendary fire-roasted tomato sauce',
+        price: '10.00'},
+        {dish: 'Chicken Parmesan',
+        desc: 'Hand-breaded baked chicken, tossed with our signature tomato sauce served on top of your choice of pasta',
+        price: '14.00'}
+    ]},
+    {title: 'Pizza',
+    items: [
+        {dish: 'Five-Cheese',
+        desc: 'Five-cheeses on top of a crust of your choice, with our signature tomato sauce',
+        price: '15.00'},
+        {dish: 'The Holy Mushroom',
+        desc: 'Four types of mushrooms on top of our savory cheese blend, your choice of red or white sauce, and covered in our white truffle aioli',
+        price: '20.00'},
+        {dish: 'Fire-Roasted Meat Lovers',
+        desc: 'Smoked italian sausage, pepperoni, and smoked bacon on our sharp cheddar blend',
+        price: '20.00'}
+    ]}
+]
