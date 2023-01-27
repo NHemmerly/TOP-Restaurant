e74b5945d230e83a209bcd8da48b228c05027ba3
Neil Hemmerly <neilhemm0@gmail.com>
2023-01-27 10:39:25 -0500

    Add basic elements of contact page to DOM

--- src/contact.js
+++ src/contact.js
@@ -3,7 +3,14 @@ import { domElement } from "./factoryDOM";
 import { clearTab } from "./clearTab";
 
 export function renderContact() {
-    const contact = domElement('div', {class: 'container-fluid', id: 'tab'})
+    const contact = domElement('div', {class: 'd-flex flex-column container-fluid main p-0 m-0', id: 'tab'},
+        domElement('div', {class: 'contact-holder align-self-end p-3 pt-4 m-3'}, 
+            domElement('div', {class: 'contact-title h3 text-light text-center'}, 'Contact Us'),
+        domElement('div', {class: 'phone-address'},
+            domElement('div', {class: 'p'})),
+        domElement('form', {class: 'contact-form'},
+        domElement('ul', {}, 
+        domElement('li', {class: 'name'})))))
     return contact;
 }
 
