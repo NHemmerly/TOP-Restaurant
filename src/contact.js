9c6f8934b1cc4680576ea6e61ac7bbf6b6c4ab61
Neil Hemmerly <neilhemm0@gmail.com>
2023-01-27 13:37:13 -0500

    Add button to contact form

--- src/contact.js
+++ src/contact.js
@@ -4,11 +4,11 @@ import { clearTab } from "./clearTab";
 
 export function renderContact() {
     const contact = domElement('div', {class: 'd-flex flex-column container-fluid main p-0 m-0', id: 'tab'},
-        domElement('div', {class: 'contact-holder align-self-end p-3 pt-4 m-3'}, 
+        domElement('div', {class: 'contact-holder align-self-center p-5 pt-4 m-3'}, 
             domElement('div', {class: 'contact-title h3 text-light text-center'}, 'Contact Us'),
         domElement('div', {class: 'phone-address'},
-            domElement('div', {class: 'p address text-light'}, 'Address: 4249 Bingo Boingo Lane, PizzaTowne, DC, 35609'),
-            domElement('div', {class: 'p phone text-light'}, 'Phone: 555-555-5555')),
+            domElement('div', {class: 'p address text-light mb-3'}, 'Address: 4249 Bingo Boingo Lane, PizzaTowne, DC, 35609'),
+            domElement('div', {class: 'p phone text-light mb-3'}, 'Phone: 555-555-5555')),
         domElement('form', {class: 'contact-form'},
         domElement('ul', {}, 
         domElement('li', {class: 'name'},
@@ -19,7 +19,9 @@ export function renderContact() {
             domElement('input', {type: 'text', class: 'form-control', id: 'Subject', name: 'Subject', placeholder: 'Enter message subject here'})),
         domElement('li', {class: 'message'},
             domElement('label', {class: 'form-label text-light mt-2', for: 'Message'}, 'Message: '),
-            domElement('textarea', {id: 'Message', name: 'Message', rows: '4', cols: '50', class: 'form-control'}))))))
+            domElement('textarea', {id: 'Message', name: 'Message', rows: '4', cols: '50', class: 'form-control', placeholder: 'Type your message here'})),
+        domElement('li', {class: 'submit-button'},
+            domElement('button', {class: 'btn btn-light mt-3', type: 'submit', id: 'contact-form'}, 'Submit Message'))))))
     return contact;
 }
 
