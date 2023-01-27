fa7c50f0c44bb9a0889408cf3d61a6c48f7fce10
Neil Hemmerly <neilhemm0@gmail.com>
2023-01-27 13:15:09 -0500

    Add basic contact form layout

--- src/contact.js
+++ src/contact.js
@@ -7,10 +7,19 @@ export function renderContact() {
         domElement('div', {class: 'contact-holder align-self-end p-3 pt-4 m-3'}, 
             domElement('div', {class: 'contact-title h3 text-light text-center'}, 'Contact Us'),
         domElement('div', {class: 'phone-address'},
-            domElement('div', {class: 'p'})),
+            domElement('div', {class: 'p address text-light'}, 'Address: 4249 Bingo Boingo Lane, PizzaTowne, DC, 35609'),
+            domElement('div', {class: 'p phone text-light'}, 'Phone: 555-555-5555')),
         domElement('form', {class: 'contact-form'},
         domElement('ul', {}, 
-        domElement('li', {class: 'name'})))))
+        domElement('li', {class: 'name'},
+            domElement('label', {class: 'form-label text-light mt-2', for: 'Name'}, 'Name: '),
+            domElement('input', {type: 'text',class: 'form-control', id: 'Name', name: 'Name', placeholder: 'Enter name here'})),
+        domElement('li', {class: 'subject'},
+            domElement('label', {class: 'form-label text-light mt-2', for: 'Subject'}, 'Subject: '),
+            domElement('input', {type: 'text', class: 'form-control', id: 'Subject', name: 'Subject', placeholder: 'Enter message subject here'})),
+        domElement('li', {class: 'message'},
+            domElement('label', {class: 'form-label text-light mt-2', for: 'Message'}, 'Message: '),
+            domElement('textarea', {id: 'Message', name: 'Message', rows: '4', cols: '50', class: 'form-control'}))))))
     return contact;
 }
 
