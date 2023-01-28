844f6132ad160ea27b97ca104690924be5ab4cb6
NeilH <neilhemm0@gmail.com>
2023-01-27 20:05:02 -0500

    Add photo background to menu tab

--- src/menu.js
+++ src/menu.js
@@ -1,10 +1,11 @@
 //Module for rendering the menu tab
 import { domElement } from "./factoryDOM";
 import { generateMenu } from "./menuGenerator";
-import { menu } from './menu-text';
 
 export function renderMenu() {
-    const main = domElement('div', {class: 'd-flex flex-column container-fluid main p-0 m-0', id: 'tab'}, generateMenu())
+    const main = domElement('div', {class: 'd-flex flex-column container-fluid main p-0 m-0', id: 'tab'}, 
+    domElement('div', {class: 'd-flex flex-column menu-background'},
+    generateMenu()))
 
     return main
 }
